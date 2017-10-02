9f47aa54e1af3bebc4d0c4ec14c8ff8087d26272
josemag1997 <jmagjp1997@gmail.com>
2017-10-02 07:52:19 -0500

    proyect

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp'),
+	pug = require('gulp-pug'),
+	watch = require('gulp-watch'),
+	browserSync = require('browser-sync').create();
+
+gulp.task('pug', ()=>
+	gulp.src('./dev/views/*.pug')//origen 
+	.pipe(pug({
+		pretty: true //para agregar saltos de linea en el html
+	}))
+	.pipe(gulp.dest('./dist/'))//destino
+	);
+gulp.task('watch', ()=>{
+	gulp.watch('./dev/views/**/*.pug', ['pug', browserSync.reload] ); //ejecuta la funcion
+})
+
+gulp.task('server', function(){
+  browserSync.init({
+    server:{
+      baseDir:'./dist'
+    }
+  })
+})
+
+gulp.task('default', ['pug'] );
+gulp.task('serve', ['watch', 'server']);
